fde312a78b9de170d3857c55fc8507f015fff197
BwisterNova <bnovalife@gmail.com>
2026-04-30 10:20:44 +0100

    first component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,25 @@
+export default function () {
+  return (
+    <div>
+      {/* Navigation */}
+      <header>
+        <nav>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </nav>
+      </header>
+
+      {/* Main Content */}
+      <main>
+        {/* Hero Section */}
+        <section></section>
+        {/* Features Section */}
+        <section></section>
+      </main>
+    </div>
+  );
+}
